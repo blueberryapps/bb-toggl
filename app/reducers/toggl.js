1a1c47086ed39c69e83a00584960d3d274eeced0
Antonín Simerský <antonin.simersky@live.com>
2017-07-22 18:10:25 +0200

    Separate data in state

--- app/reducers/toggl.js
+++ app/reducers/toggl.js
@@ -3,7 +3,8 @@ import { LOGIN_SUCCESS, LOGOUT } from '../actions/toggl';
 const initialState = {
   apiToken: null,
   isLogged: false,
-  response: null,
+  projects: null,
+  timeEntries: null,
 };
 
 export const toggl = (state = initialState, action) => {
@@ -13,7 +14,8 @@ export const toggl = (state = initialState, action) => {
         ...state,
         apiToken: action.payload.data.api_token,
         isLogged: true,
-        response: action.payload
+        projects: action.payload.data.projects,
+        timeEntries: action.payload.data.time_entries
       };
     case LOGOUT:
       return {
