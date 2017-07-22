45e020e3c0afdca1808500ac94eb4b68f2077ed9
Milos Kolcak <milos.kolcak@gmail.com>
2017-07-23 00:37:09 +0200

    Fix group date format

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -3,6 +3,6 @@ import moment from 'moment';
 export const secondsToHours = (seconds) => moment('1900-01-01').startOf('day').seconds(seconds).format('H[h] mm[m] ss[s]');
 
 export const groupByTimeEntryDate = (timeEntries) => timeEntries.reduce((acc, cur) => {
-  const date = moment(cur.start).format('DD.MM.YYYY');
+  const date = moment(cur.start).format('DD. MMMM YYYY');
   return { ...acc, [date]: [...acc[date] || [], cur] };
 }, {});
