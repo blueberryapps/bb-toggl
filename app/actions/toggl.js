90ea1b56f928d00d25f4e9cd0849e2b572ffd93c
Antonín Simerský <antonin.simersky@live.com>
2017-07-23 02:01:35 +0200

    Start tracking fixes Add stop tracking

--- app/actions/toggl.js
+++ app/actions/toggl.js
@@ -5,6 +5,7 @@ export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGOUT = 'LOGOUT';
 export const TRACKING_START = 'TRACKING_START';
 export const TRACKING_START_SUCCESS = 'TRACKING_START_SUCCESS';
+export const TRACKING_STOP = 'TRACKING_STOP';
 
 export const login = (username, password) => {
   const options = {
@@ -45,6 +46,28 @@ export const startTracking = (trackingDetails) => ({ getState }) => {
   return {
     type: TRACKING_START,
     payload: fetch('https://www.toggl.com/api/v8/time_entries/start', options)
-      .then((r) => r.json())
+      .then(async (r) => {
+        const { data } = await r.json();
+        return data;
+      })
+  };
+};
+
+export const stopTracking = (timeEntryId) => ({ getState }) => {
+  const options = {
+    method: 'PUT',
+    headers: {
+      Authorization: `Basic ${new Buffer(`${getState().toggl.apiToken}:api_token`, 'utf8').toString('base64')}`,
+      'Content-Type': 'application/json',
+    },
+  };
+
+  return {
+    type: TRACKING_STOP,
+    payload: fetch(`https://www.toggl.com/api/v8/time_entries/${timeEntryId}/stop`, options)
+      .then(async (r) => {
+        const { data } = await r.json();
+        return data;
+      })
   };
 };
